1f238a89fc26fe8f0546428246c33aa83b5c7b75
Lluke18 <lucianlesustefan@gmail.com>
2026-04-21 21:56:59 +0300

    Squashed commit of the following:
    commit d8e353abca5a3cf53780b7d0d5c51edee5770ee7
    Author: Lluke18 <lucianlesustefan@gmail.com>
    Date:   Tue Apr 21 21:56:23 2026 +0300
    
        added light
    
    commit d28de72e309abd47d7301d808c6336618393424b
    Merge: 35af467 a41ba97
    Author: Lluke18 <lucianlesustefan@gmail.com>
    Date:   Tue Apr 21 18:35:55 2026 +0300
    
        Merge branch 'main' into luci-dev-1.2.1
    
    commit 35af467bf0c0302b17494441378b692c91b06964
    Author: Lluke18 <lucianlesustefan@gmail.com>
    Date:   Tue Apr 21 18:34:53 2026 +0300
    
        modified the spawn point to be near the window

--- Scripts/room.gd
+++ Scripts/room.gd
@@ -6,7 +6,6 @@ extends Node
 
 @export var mirror : InteractArea
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	InventoryManager.add_item(InventoryManager.KEY)
 	#pt tranzitia de la 3d, ca mouse-ul sa se vada din nou in 2d
@@ -17,5 +16,5 @@ func _ready() -> void:
 
 func _on_mirror_change():
 	print("SWITCHING!")
-	InteractionManager.unregister_area(mirror)
+	InteractionManager.unregister_area(mirror) # so it doesnt cause errors in 3d
 	SceneChanger.change_scene_to_path(room_3d_path)
