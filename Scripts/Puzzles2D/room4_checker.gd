0947259fb6c23ba491dbcdb300a5c10dddbe3afe
Dani Munteanu <daniel.munteanu@student.upt.ro>
2026-05-07 16:37:40 +0300

    Added tooltip to books

--- Scripts/Puzzles2D/room4_checker.gd
+++ Scripts/Puzzles2D/room4_checker.gd
@@ -1,7 +1,25 @@
-extends Node
+extends Control
 
 @onready var star: Button = $Star
+@onready var books: Button = $Books
+
+@onready var tooltip_scene = preload("res://UI/Tooltip.tscn")
+var tooltip: Tooltip
+
+func _process(delta: float) -> void:
+	if tooltip:
+		tooltip.global_position = get_global_mouse_position() + Vector2(0, -80)
 
 func _ready() -> void:
 	if PuzzleManager.calendar_solved == true:
 		star.disabled = true
+		
+func _on_books_mouse_entered() -> void:
+	tooltip = tooltip_scene.instantiate()
+	tooltip.set_text("Maybe those colors hint to something...")
+	add_child(tooltip)
+	
+func _on_books_mouse_exited() -> void:
+	if tooltip:
+		tooltip.queue_free()
+		tooltip = null
