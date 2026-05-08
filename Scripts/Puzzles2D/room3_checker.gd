55966b9aece825ab97f74e9afc2884c14543e84d
Dani Munteanu <daniel.munteanu@student.upt.ro>
2026-05-08 13:36:38 +0300

    Added skippable intro and outro videos

--- Scripts/Puzzles2D/room3_checker.gd
+++ Scripts/Puzzles2D/room3_checker.gd
@@ -1,10 +1,13 @@
-extends Node
+extends Control
 
 @onready var death: Button = $Death
+@onready var grimoire: Button = $Grimoire
+@onready var pisi: Button = $"../Pisi"
+
 @onready var sfx_player: AudioStreamPlayer2D = $"../../SfxPlayer"
 @onready var sfx_player_2: AudioStreamPlayer2D = $"../../SfxPlayer2"
 
-
+@onready var cinematic_player: CinematicPlayer = $"../CinematicPlayer"
 
 func _ready() -> void:
 	if PuzzleManager.complete_puzzles[PuzzleManager.puzzles.DEATH] == true:
@@ -12,17 +15,29 @@ func _ready() -> void:
 
 func _on_grimoire_pressed() -> void:
 	if PuzzleManager.completed_puzzles == PuzzleManager.number_of_puzzles - 1:
+		disable_buttons()
 		PuzzleManager.finish_puzzle(PuzzleManager.puzzles.WORLD)
 		sfx_player.play()
 		TextManager.show_once("World_completed", [
 			"A mirror only shatters when its work is done!"
 		])
 		SignalBus.world_completed.emit()
-		await get_tree().create_timer(6).timeout
-		SceneChanger.change_scene_to_path("res://Rooms/Room2.tscn")
 		sfx_player_2.play()
+		await get_tree().create_timer(2).timeout
+		
+		cinematic_player.show()
+		cinematic_player.play_outro()
+		
+		await cinematic_player.cinematic_finished
+		
+		DirAccess.remove_absolute("user://SaveFile.tres")
+		SceneChanger.change_scene_to_path("res://scenes/2d/main_menu.tscn")
 	else:
 		TextManager.show_once("World_empty", [
 			"The Grimoire is silent. I need to find the rest of the cards before I even think about starting the spell."
 		])
-	
+		
+func disable_buttons():
+	death.disabled = true
+	grimoire.disabled = true
+	pisi.disabled = true
--- scenes/2d/main_menu.gd
+++ scenes/2d/main_menu.gd
@@ -2,17 +2,15 @@ extends CanvasLayer
 
 @onready var options_panel = $OptionsPanel
 @onready var continue_button: Button = $TextureRect/ButtonManager/Continue
+@onready var cinematic_player: CinematicPlayer = $CinematicPlayer
+
 @export var start_scene_path : String
 @export var options_menu_path : String 
 
-@onready var cutscene_node = $Cutscene 
-@onready var anim_player: AnimationPlayer = $Cutscene/CanvasLayer/AnimationPlayer
-@onready var skip_button = $Cutscene/CanvasLayer/Control/Button
-
 func _ready() -> void:
+	AudioManager.music_player.play()
 	#Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	cutscene_node.hide()
-	skip_button.hide()
+	cinematic_player.hide()
 	if FileAccess.file_exists("user://SaveFile.tres") == false:
 		continue_button.disabled = true
 		continue_button.modulate.a = 0.5
@@ -22,17 +20,16 @@ func _on_start_pressed() -> void:
 	DirAccess.remove_absolute("user://SaveFile.tres")
 	InventoryManager.reset_data()
 	PuzzleManager.reset_data()
-	$TextureRect.hide()       
-	cutscene_node.show()
-	skip_button.show()
-	skip_button.pressed.connect(_on_skip_pressed)
-	anim_player.play("intro")
-	await anim_player.animation_finished
+	   
+	cinematic_player.show()
+	cinematic_player.play_intro()
+	
+	await cinematic_player.cinematic_finished
+	
 	SceneChanger.change_scene_to_path(start_scene_path)
 
 func _on_options_pressed() -> void:
 	options_panel.visible = true
-	SceneChanger.change_scene_to_path(options_menu_path)
 
 func _on_continue_pressed() -> void:
 	InventoryManager.load_data()
@@ -41,8 +38,3 @@ func _on_continue_pressed() -> void:
 
 func _on_quit_game_pressed() -> void:
 	get_tree().quit()
-
-
-func _on_skip_pressed() -> void:
-	anim_player.stop()
-	SceneChanger.change_scene_to_path(start_scene_path)
